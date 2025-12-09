c12e6fd9a2573cf6cb9e4b40ba115c6f7887d21d
kolleennz <kolleennz@gmail.com>
2025-12-09 19:05:21 +0100

    Yet again

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ variable vpc_cidr_block{}
 variable subnet_cidr_block{}
 variable avail_zone{}
 variable env_prefix {}
-
+variable my_ip {}
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
 
@@ -15,10 +15,61 @@ resource "aws_vpc" "myapp-vpc" {
 
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block       = var.cidr_block[1].cidr_block
+  cidr_block       = var.subnet_cidr_block
   availability_zone = var.avail_zone
 
   tags = {
     Name = "${var.env_prefix}-subnet-1"
+  }
+}
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-route-table"
+  }  
+}
+
+resource "aws_route_table_association" "myapp-rtb-assoc-1" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name = "${var.env_prefix}-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+  description = "Security group for ${var.env_prefix} application"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
